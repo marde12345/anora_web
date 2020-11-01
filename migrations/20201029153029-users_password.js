8acd27349eba5828e73a8ffd30e066dd2f2cdf54
Marde Fasma <mardefasma123up@gmail.com>
2020-11-01 14:04:39 +0700

    Migration : add firstname and lastname

--- migrations/20201029153029-users_password.js
+++ migrations/20201029153029-users_password.js
@@ -6,6 +6,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('users', password);
+    await queryInterface.removeColumn('users', 'password');
   }
 };
